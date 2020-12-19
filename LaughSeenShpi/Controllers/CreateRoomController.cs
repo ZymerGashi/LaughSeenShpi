49122923ca0d3d8b1c7f1b9a00ed0eeeed0a84df
Zymer Gashi <zymer.gashii@outlook.com>
2020-12-19 14:39:06 +0100

    Create the CreateRoom controller and its view

---
+++ LaughSeenShpi/Controllers/CreateRoomController.cs
@@ -0,0 +1,26 @@
+﻿using LaughSeenShpi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaughSeenShpi.Controllers
+{
+    public class CreateRoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Room room)
+        {
+
+            return View();
+        }
+
+     
+    }
+}
